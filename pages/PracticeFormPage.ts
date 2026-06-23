a2cce2467a7e5b9423d605885b3d5cad35852c0e
ThuyTran <tranthuy139@gmail.com>
2026-06-23 22:39:09 +0700

    push code day9-practice-form

--- pages/PracticeFormPage.ts
+++ pages/PracticeFormPage.ts
@@ -3,16 +3,16 @@ export class PracticeFormPage {
     readonly txtFirstName : Locator;
     readonly txtLastName : Locator;
     readonly txtEmail : Locator; 
-    rdGender: string = '//*[text()="@param"]';
+    rdGender: string = 'xpath=//*[text()="@param"]';
     readonly txtMobile : Locator;
     readonly txtDateOfBirth : Locator;
     readonly txtSubjects : Locator;
-    readonly chkHobbies : Locator;
+    chkHobbies : string  = 'xpath=//label[text()="@param"]';
     readonly txtPicture : Locator;
     readonly btnChooseFile : Locator;
     readonly txtCurrentAddress : Locator;
-    readonly ddlState : Locator ;
-    readonly ddlCity : Locator;
+    readonly cbxState : Locator ;
+    readonly cbxCity : Locator;
     readonly btnSubmit : Locator;
     constructor(public readonly page: Page) {
         this.txtFirstName = page.locator("#firstName");
@@ -21,33 +21,30 @@ export class PracticeFormPage {
         this.txtMobile = page.locator("#userNumber");
         this.txtDateOfBirth = page.locator("#dateOfBirthInput");
         this.txtSubjects = page.locator("#subjectsInput");
-        this.chkHobbies = page.locator("input[type='checkbox']");
         this.txtPicture = page.locator("#uploadPicture");
         this.btnChooseFile = page.locator("button#uploadButton");
         this.txtCurrentAddress = page.locator("#currentAddress");
-        this.ddlState = page.locator("#state");
-        this.ddlCity = page.locator("#city");
+        this.cbxState = page.locator('xpath=//*[@id="state"]//input');
+        this.cbxCity = page.locator('xpath=//*[@id="city"]//input');
         this.btnSubmit = page.locator("#submit");
     }
     async goto() {
         await this.page.goto("/automation-practice-form");
     }   
-    async inputData(firstName: string, lastName: string, email: string, gender: string, mobile: string, dateOfBirth: string, subjects: string, hobbies: string[], picturePath: string, currentAddress: string = '', state: string, city: string) {
+    async inputData(firstName: string, lastName: string, email: string, gender: string, mobile: string, dateOfBirth: string, subjects: string, hobbies: string[], picture: string, currentAddress: string = '', state: string, city: string) {
         await this.txtFirstName.fill(firstName);
         await this.txtLastName.fill(lastName);
         await this.txtEmail.fill(email);
         await this.page.click(this.rdGender.replace('@param', gender));
         await this.txtMobile.fill(mobile);
         await this.inputDateOfBirth(dateOfBirth);   
-        await this.txtSubjects.fill(subjects);
-        await this.txtSubjects.press('Enter');
-        for (const hobby of hobbies) {
-            await this.page.getByLabel(hobby).check();
-        }
+        await this.inputSubjects(subjects);
+        await this.inputHobbies(hobbies);
+        const picturePath = process.cwd() + '\\testcase\\data\\' + picture;
         await this.txtPicture.setInputFiles(picturePath);
         await this.txtCurrentAddress.fill(currentAddress);
-        await this.ddlState.selectOption(state);
-        await this.ddlCity.selectOption(city);
+        await this.cbxState.fill(state);
+        await this.cbxCity.fill(city);
         await this.btnSubmit.click();
     }
     async submit() {
@@ -69,5 +66,18 @@ export class PracticeFormPage {
         await this.page.locator('.react-datepicker__year-select').selectOption(year);
         await this.page.locator('.react-datepicker__month-select').selectOption(month);
         await this.page.locator(`.react-datepicker__day--0${day}`).click();
+    }
+    async inputSubjects(subjects: string) {
+        const subjectList = subjects.split(',').map(subject => subject.trim());
+        for (const subject of subjectList) {
+            await this.txtSubjects.fill(subject);
+            await this.txtSubjects.press('Enter');
+        }
+    }
+    async inputHobbies(hobbies: string[]) { 
+        const hobbyList = hobbies.map(hobby => hobby.trim());
+        for (const hobby of hobbyList) {
+            await this.page.click(this.chkHobbies.replace('@param', hobby));
+        }
     }
 }
--- tests/PracticeForm.spec.ts
+++ tests/PracticeForm.spec.ts
@@ -1,9 +1,9 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { PracticeFormPage } from '../pages/PracticeFormPage.js';
 import { ThanksForSubmittingPage } from '../pages/ThanksForSubmittingPage.js';
 test.describe('Practice Form Tests', () => {
     let practiceFormPage : PracticeFormPage;
-    let thanksForSubmittingPage;    
+    let thanksForSubmittingPage : ThanksForSubmittingPage;    
     test.beforeEach(async ({page}) => {
         practiceFormPage = new PracticeFormPage(page);
         thanksForSubmittingPage = new ThanksForSubmittingPage(page);
@@ -15,13 +15,45 @@ test.describe('Practice Form Tests', () => {
         const email: string = 'Thuy@vcb.com';
         const gender: string = 'Male';
         const mobile: string = '0123456789';
-        const dateOfBirth: string = '01 January 2000';
-        const subjects: string = 'Maths';
-        const hobbies: string[] = ['Sports', 'Reading'];
-            const picturePath: string = 'tests/fixtures/upload.txt';
-        const currentAddress: string = '123 Main St, Anytown, USA';
+        const dateOfBirth: string = '01 January,2000';
+        const subjects: string = 'Maths, Physics, Chemistry';
+        const hobbies: string[] = ['Sports', 'Reading', 'Music'];
+        const picture = 'THUYTRAN.jpg';
         const state: string = 'NCR';
         const city: string = 'Delhi';
-        await practiceFormPage.inputData(firstName, lastName, email, gender, mobile, dateOfBirth, subjects, hobbies, picturePath, currentAddress, state, city);
-    });
+        await practiceFormPage.inputData(firstName, lastName , email, gender, mobile, dateOfBirth, subjects, hobbies, picture, '', state, city);
+        const actualStudentName: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Student Name');
+        const expectedStudentName: string = firstName + ' ' + lastName;
+        expect(actualStudentName.trim()).toBe(expectedStudentName);
+        
+        const actualStudentEmail: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Student Email');
+        expect(actualStudentEmail.trim()).toBe(email);   
+
+        const actualGender: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Gender');
+        expect(actualGender.trim()).toBe(gender);
+
+        const actualMobile: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Mobile');
+        expect(actualMobile.trim()).toBe(mobile);   
+
+        const actualDateOfBirth: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Date of Birth');
+        const firstSpace = dateOfBirth.indexOf(" ");
+        const secondSpace = dateOfBirth.indexOf(" ", firstSpace + 1);
+        const expectedDateOfBirth: string = dateOfBirth.replace(dateOfBirth[secondSpace], ","); // Remove the second space to match the format in the confirmation page
+
+        expect(actualDateOfBirth.trim()).toBe(expectedDateOfBirth); 
+
+        const actualSubjects: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Subjects');
+        expect(actualSubjects.trim()).toBe(subjects);
+
+        const actualHobbies: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Hobbies');
+        const expectedHobbies: string = hobbies.join(', ');
+        expect(actualHobbies.trim()).toBe(expectedHobbies); 
+
+        const actualPicture: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Picture');
+        expect(actualPicture.trim()).toBe(picture);     
+
+        const actualAddress: string = await thanksForSubmittingPage.getValueByLabel(thanksForSubmittingPage.lblValue, 'Address');
+        expect(actualAddress.trim()).toBe(currentAddress);
+
+    }); 
 });
